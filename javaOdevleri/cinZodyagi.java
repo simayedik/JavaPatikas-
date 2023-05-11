6e42c3e30cc00a415902a380a29451a802d83924
Simayedik <108196211+simayedik@users.noreply.github.com>
2023-05-11 11:45:07 +0300

    Add files via upload

---
+++ javaOdevleri/cinZodyagi.java
@@ -0,0 +1,56 @@
+package javaOdevleri;
+
+import java.util.Scanner;
+
+public class cinZodyagi {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int mounth,z;
+        System.out.print("Doğduğunuz Yılınızı Giriniz:");
+        mounth = inp.nextInt();
+
+            z=mounth%12;
+            switch (z){
+                case 0:
+                    System.out.println("Maymun");
+                    break;
+                case 1:
+                    System.out.println("Horoz");
+                    break;
+                case 2:
+                    System.out.println("Köpek");
+                    break;
+                case 3:
+                    System.out.println("Domuz");
+                    break;
+                case 4:
+                    System.out.println("Fare");
+                    break;
+                case 5:
+                    System.out.println("Öküz");
+                    break;
+                case 6:
+                    System.out.println("Kaplan");
+                    break;
+                case 7:
+                    System.out.println("Tavşan");
+                    break;
+                case 8:
+                    System.out.println("Ejderha");
+                    break;
+                case 9:
+                    System.out.println("Yılan");
+                    break;
+                case 10:
+                    System.out.println("At");
+                    break;
+                case 11:
+                    System.out.println("Koyun");
+                    break;
+            }
+
+    }
+}
+
+
