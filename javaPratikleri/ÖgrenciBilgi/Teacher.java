e9ec324aedf017ffc37b4f42543181aeaa85d9e6
Simayedik <108196211+simayedik@users.noreply.github.com>
2023-06-14 13:06:53 +0300

    Add files via upload

---
+++ javaPratikleri/ÖgrenciBilgi/Teacher.java
@@ -0,0 +1,13 @@
+package javaPratikleri.ÖgrenciBilgi;
+
+public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+}
